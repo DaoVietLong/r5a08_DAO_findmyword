c29e78c3947942db6dc4a0302fcc26f6aecedcc0
DaoVietLong <daovietlong10@gmail.com>
2024-10-10 14:50:33 +0200

    adding part-correct point

--- src/main/java/r5a08_findmyword/Score.java
+++ src/main/java/r5a08_findmyword/Score.java
@@ -17,5 +17,13 @@ public class Score {
         if (this.word.charAt(index) == myStr.charAt(index)){
             point = Letter.CORRECT;
         }
+        else {
+            for (int i = 0; i < this.word.length(); i++) {
+                if (this.word.charAt(i) == myStr.charAt(index)) {
+                    point = Letter.PART_CORRECT;
+                }
+            }
+        }
+        e
     }
 }
